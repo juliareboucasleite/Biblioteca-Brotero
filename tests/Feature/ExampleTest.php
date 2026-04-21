9fbb1d55609ef8fcdf1ccd92d1ee92bdd9deda19
Julia Rebouças <69313019+juliareboucasleite@users.noreply.github.com>
2026-04-21 16:10:55 +0100

    chore: Adicionar .gitignore e remover arquivos de testes
    - Criado .gitignore para proteger arquivos sensíveis (.env, node_modules, vendor, etc)
    - Removidos diretórios de testes e configurações desnecessárias
    - Projeto agora mais limpo e seguro

--- tests/Feature/ExampleTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use function Pest\Laravel\get;
-
-test('a raiz redirecciona para o catálogo da biblioteca', function () {
-    $response = get(route('home'));
-
-    $response->assertRedirect('/biblioteca');
-});
